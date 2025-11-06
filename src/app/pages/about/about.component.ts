af6795e6eab62efd8f3618f0da6fb635550a60d9
Julia Dobrynina <JDobrynina@yandex.ru>
2025-11-06 11:31:09 +0800

    version 3.0.1

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { from } from 'rxjs';
+
+import { ElectronService } from '$services/electron.service';
 
 @Component({
   selector: 'about',
   standalone: true,
+  imports: [
+    AsyncPipe,
+  ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {}
+export class AboutComponent {
+  protected electronService = inject(ElectronService);
+
+  protected version = computed(() => from(this.electronService.ipcRenderer.invoke('getVersion')));
+}
